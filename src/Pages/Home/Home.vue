b25efe0408f35ef1ef047547bfff0291a502d64e
Giovanni <1300687449@qq.com>
2019-08-09 09:07:56 +0800

    login、MainMenu、Factory by Giovanni at 8-8

--- src/Pages/Home/Home.vue
+++ src/Pages/Home/Home.vue
@@ -1,25 +1,49 @@
 <template>
   <div>
-    主页
+    <el-container>
+      <el-header>
+        <MHeader/>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">
+          <NavSide/>
+        </el-aside>
+        <el-main>
+          <RouterView></RouterView>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
   import {mapState} from 'vuex'
+  import MHeader from '../../components/MHeader/MHeader'
+  import NavSide from '../../components/NavSide/NavSide'
 
   export default {
+    components: {
+      MHeader,
+      NavSide
+    },
     data () {
       return {}
     },
     computed: {
-      ...mapState(['User'])
+      ...mapState(['User', 'FactoryId'])
     },
     mounted () {
-      console.log(this.User)
+      if (this.FactoryId === 0) {
+        this.$store.dispatch('setFactoryList')
+      }
     }
   }
 </script>
 
 <style scoped>
-
+  .el-header {
+    width: 100%;
+    background-color: #090723;
+    color: #ffffff;
+  }
 </style>
